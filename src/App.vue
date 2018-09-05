602ec4aadfac949d51f559ae73f34d7c1a46e768
zlumer <zlumer@gmail.com>
2018-09-05 11:52:40 +0300

    qr code display

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
-<div>Hello</div>
+<div>
+	<Qr :qrcode="data"></Qr>
+	<button @click="onclick">click</button>
+</div>
 </template>
 
 <script lang="ts">
 
 import Vue from "vue"
+import Qr from "./QrImage.vue"
 
-export default Vue.extend({})
+
+let App = Vue.extend({
+	data: function ()
+	{
+		return {
+			data: ""
+		}
+	},
+	methods: {
+		onclick: function (data: string)
+		{
+			this.data += String.fromCharCode(Math.floor(Math.random() * 25) + "a".charCodeAt(0))
+		}
+	},
+	components: {
+		Qr
+	}
+})
+export default App
 
 </script>
 
---
+++ src/QrImage.vue
@@ -0,0 +1,32 @@
+<template>
+	<img :src="dataUrl" alt="">
+</template>
+
+<script lang="ts">
+
+import Vue from "vue"
+import qr from "qrcode"
+
+let App = Vue.extend({
+	data: function ()
+	{
+		return {
+			dataUrl: "",
+		}
+	},
+	props: {
+		qrcode: String
+	},
+	watch: {
+		qrcode: async function ()
+		{
+			this.dataUrl = await qr.toDataURL(this.qrcode)
+		}
+	}
+})
+export default App
+
+</script>
+
+<style>
+</style>
