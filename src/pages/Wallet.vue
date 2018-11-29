83a43a43c6ab49ac818d0bd1ef69499577b89d10
zlumer <zlumer@gmail.com>
2018-11-30 01:10:40 +0300

    duplicate keys fix

--- src/pages/Wallet.vue
+++ src/pages/Wallet.vue
@@ -9,11 +9,20 @@
 
 <script lang="ts">
 import Vue from 'vue'
+
+interface ITransaction
+{
+	hash: string
+	from: string
+	to: string
+	value: string
+}
+
 export default Vue.extend({
 	data()
 	{
 		return {
-			txs: []
+			txs: [] as ITransaction[]
 		}
 	},
 	computed: {
@@ -28,7 +37,9 @@ export default Vue.extend({
 		let data = await fetch(url)
 		let json = await data.json()
 		console.log(json)
-		this.txs = json.result
+		let unique = {} as {[hash:string]: boolean}
+		let items = [].concat(json.result) as ITransaction[]
+		this.txs = items.filter(x => unique[x.hash] ? false : (unique[x.hash] = true))
 	},
 })
 </script>
