330e69a29760295f19fcb9b8ec8455eb80e631bb
zlumer <zlumer@gmail.com>
2018-11-30 02:48:19 +0300

    tx push

--- src/pages/NewTransfer.vue
+++ src/pages/NewTransfer.vue
@@ -3,27 +3,35 @@
 		To: <input type="text" ref="to" name="to" :disabled="signing">
 		<br/>
 		<button type="submit" @click="onButton" :disabled="signing">SIGN TX</button>
-		<remote-call v-if="signing" method="signTransferTx" :params="txJson" @result="onResult"></remote-call>
-		<button type="submit" @click="onCancel" v-if="signing">Cancel</button>
+		<span v-if="signing && loading">loading...</span>
+		<remote-call v-if="signing && !loading" method="signTransferTx" :params="txJson" @result="onResult"></remote-call>
+		<button type="submit" @click="onCancel" v-if="signing && !loading">Cancel</button>
 	</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import RemoteCall from '../components/RemoteCall.vue'
+import * as eth from "../web3"
 
 export default Vue.extend({
 	data()
 	{
 		return {
-			signing: false
+			signing: false,
+			loading: false,
+			nonce: NaN,
 		}
 	},
 	computed: {
+		address: function()
+		{
+			return this.$route.params.address
+		},
 		wallet: function()
 		{
 			let wallets = JSON.parse(localStorage.getItem('wallets') || '[]') as {address:string}[]
-			return wallets.find(x => x.address == this.$route.params.address)
+			return wallets.find(x => x.address == this.address)
 		},
 		txJson: function()
 		{
@@ -33,22 +41,26 @@ export default Vue.extend({
 		tx: function()
 		{
 			return {
-				from: this.$route.params.address,
+				from: this.address,
 				to: (this.$refs.to as HTMLInputElement).value,
-				nonce: 0,
+				nonce: this.nonce,
 				gasPrice: 3 * 1e12,
 				value: 0,
 			}
 		}
 	},
 	methods: {
-		onButton()
+		async onButton()
 		{
 			this.signing = true
+			this.loading = true
+			this.nonce = await eth.getNonce(this.address)
+			this.loading = false
 		},
 		onCancel()
 		{
 			this.signing = false
+			this.loading = false
 		},
 		onResult(signedTx: string)
 		{
--- src/pages/PushTx.vue
+++ src/pages/PushTx.vue
@@ -2,16 +2,42 @@
 	<div>
 		Push TX
 		<span>{{ tx }}</span>
+		<span v-if="loading">Pushing...</span>
+		<span v-if="error">Error! {{ error.name }} {{ error.message }}</span>
+		<span v-if="result">Success! pushed tx with hash: {{ result.transactionHash }}</span>
 	</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import * as eth from "../web3"
+import { TransactionReceipt } from 'web3/types'
+
 export default Vue.extend({
+	data()
+	{
+		return {
+			loading: true,
+			error: undefined as any as Error,
+			result: undefined as any as TransactionReceipt,
+		}
+	},
 	props: {
 		tx: {
 			type: String,
 			required: true,
+		}
+	},
+	mounted: async function ()
+	{
+		try
+		{
+			let result = await eth.sendTx(this.tx)
+			this.result = result
+		}
+		catch (e)
+		{
+			this.error = e
 		}
 	}
 })
