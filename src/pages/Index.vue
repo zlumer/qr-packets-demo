9a9a422222ef66ed5e05e66158d5030c0cdbe990
zlumer <zlumer@gmail.com>
2018-11-29 21:53:22 +0300

    pages

---
+++ src/pages/Index.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<router-link to="/login">Login with QR</router-link>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+	
+})
+</script>
+
---
+++ src/pages/Login.vue
@@ -0,0 +1,14 @@
+<template>
+	<QrExchange :qrs="[]" intro="Scan QR code to log in"></QrExchange>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import QrExchange from "../components/QrExchange.vue"
+
+export default Vue.extend({
+	components: {
+		QrExchange,
+	}
+})
+</script>
