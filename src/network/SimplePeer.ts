76d5c54c75a0cc552cead1093b5a71aaedaad0cb
zlumer <zlumer@gmail.com>
2018-12-12 21:56:49 +0300

    improvements

--- src/network/SimplePeer.ts
+++ src/network/SimplePeer.ts
@@ -5,9 +5,11 @@ namespace global
 	export let wrtc: any
 }
 
+export type SignalData = { type?: string, sdp?: any, candidate?: any }
+
 export function createPeer(opts: SimplePeer.Options)
 {
-	if (global.wrtc)
+	if (global && global.wrtc)
 		opts.wrtc = global.wrtc
 	
 	return new SimplePeer(opts)
