83a42a62b38f4a716e926c7d27efdb17bfdd0f45
zlumer <zlumer@gmail.com>
2018-12-19 01:02:52 +0300

    added error message on wallet page

--- src/pages/Wallet.vue
+++ src/pages/Wallet.vue
@@ -2,9 +2,10 @@
 	<div>
 		<h1>{{address}}</h1>
 		<router-link :to="{name:'newtx', params:{ address: address }}">new tx</router-link>
-		<ul>
+		<ul v-if="!error">
 			<li :key="tx.hash" v-for="tx in txs">hash: {{ tx.hash.substr(0, 10) + "..." }}, from: {{ tx.from.substr(0, 10) + "..." }}, to: {{ tx.to.substr(0, 10) + "..." }}, amount: {{ (tx.value / 1e18).toFixed(4) }}</li>
 		</ul>
+		<span v-if="error">ERROR LOADING TRANSACTIONS: {{ error }}</span>
 	</div>
 </template>
 
@@ -23,7 +24,8 @@ export default Vue.extend({
 	data()
 	{
 		return {
-			txs: [] as ITransaction[]
+			txs: [] as ITransaction[],
+			error: '',
 		}
 	},
 	computed: {
@@ -38,6 +40,11 @@ export default Vue.extend({
 		let data = await fetch(url)
 		let json = await data.json()
 		console.log(json)
+		if (json.status != '1')
+		{
+			this.error = json.result
+			return
+		}
 		let unique = {} as {[hash:string]: boolean}
 		let items = [].concat(json.result) as ITransaction[]
 		this.txs = items.filter(x => unique[x.hash] ? false : (unique[x.hash] = true))
