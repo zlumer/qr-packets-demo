b1a40eb43a1394b8c4df7f569373b6385c32b7dc
zlumer <zlumer@gmail.com>
2018-12-25 18:35:28 +0300

    refactored store to separate files

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,110 +1,30 @@
 import Vuex, { Store } from "vuex"
 
-import { IWallet, IBlockchainSymbol } from "./interop"
-import { StoreOptions, Dispatcher, Mutator } from "./vuex-type-ext"
-import { TypedBlockchains, typedBlockchains } from "src/blockchains"
-import { IBlockchain } from "src/blockchains/IBlockchain"
+import { StoreOptions, MergeMultiple, Merge } from "./vuex-type-ext"
+
+import { options as main, Store as StoreMain, IState as StateMain } from "./main"
 
 export function createStore()
 {
-	return new Vuex.Store<IState>({
-		state: {
-			wallets: [],
-			currentWallet: null,
-			txToPush: null,
-			webrtc: {
-				outgoingId: 1,
-				connected: false
-			}
-		},
-		getters: {
-			txHash: (state, getters) => (tx, bc) =>
-			{
-				return getters.blockchains[bc](state.currentWallet!.chainId).getTxHash(tx)
-			},
-			txToPushHash: (state, getters) =>
-			{
-				let tx = state.txToPush
-				if (!tx)
-					return undefined
-				
-				return getters.txHash(tx.tx, tx.wallet.blockchain)
-			},
-			blockchains: (state, getters) => typedBlockchains,
-			currentBlockchain: (state, getters) => getters.blockchains[state.currentWallet!.blockchain](state.currentWallet!.chainId)
-		},
-		mutations: {
-			setWalletList: (state, payload) =>
-			{
-				console.log(`SET_WALLET_LIST: `, payload)
-				state.wallets = payload.wallets
-			},
-			setCurrentWallet: (state, payload) => state.currentWallet = payload.wallet,
-			setTxToPush: (state, payload) => state.txToPush = payload,
-			resetTxToPush: state => state.txToPush = null,
-			webrtcIncId: state => state.webrtc.outgoingId++,
-			webrtcResetId: state => state.webrtc.outgoingId = 1,
-			webrtcConnected: state =>
-			{
-				console.log('MUTATION: webrtcConnected')
-				state.webrtc.connected = true
-			},
-			webrtcDisconnected: state => state.webrtc.connected = false,
-		},
-		actions: {
-			webrtcConnected(store)
-			{
-				store.commit('webrtcConnected')
-			},
-			webrtcDisconnected(store)
-			{
-				store.commit('webrtcDisconnected')
-				store.commit('webrtcResetId')
-			}
-		}
-	} as StoreOptions<IState, MutationPayloadMap, ActionPayloadMap, GettersReturnMap>)
+	return new Vuex.Store<IState>(mergeOptions(main))
 }
 
-export type Store = Dispatcher<ActionPayloadMap> & Mutator<MutationPayloadMap> & {
-	readonly state: IState
-	readonly getters: GettersReturnMap
-}
+export type Store = StoreMain
+export type IState = StateMain
 
-export interface IState
+
+
+type SOU = StoreOptions<unknown, unknown, unknown, unknown>
+function mergeOptions2<T extends SOU, U extends SOU>(opts1: T, opts2: U): Merge<T, U>
 {
-	wallets: IWallet[]
-	currentWallet: IWallet | null
-	txToPush: {
-		tx: string
-		wallet: IWallet
-	} | null
-	webrtc: {
-		outgoingId: number
-		connected: boolean
-	}
+	return Object.keys(opts1).concat(Object.keys(opts2)).map(key => [key, {
+		...opts1[key as keyof typeof opts1],
+		...opts2[key as keyof typeof opts2]
+	}]).reduce((prev, [key, val]) => ({ ...prev, [key as keyof Merge<T,U>]: val } as Merge<T,U>), {} as Merge<T,U>)
 }
-
-type MutationPayloadMap = {
-	setWalletList: { wallets: IWallet[] }
-	setCurrentWallet: { wallet: IWallet }
-
-	setTxToPush: { tx: string, wallet: IWallet }
-	resetTxToPush: undefined
-
-	webrtcIncId: undefined
-	webrtcResetId: undefined
-	webrtcConnected: undefined
-	webrtcDisconnected: undefined
-}
-
-type ActionPayloadMap = {
-	webrtcConnected: undefined
-	webrtcDisconnected: undefined
-}
-
-type GettersReturnMap = {
-	txHash: (tx: string, blockchain: IBlockchainSymbol) => string
-	txToPushHash: string
-	blockchains: TypedBlockchains,
-	currentBlockchain: IBlockchain<unknown, unknown>
+function mergeOptions<T1 extends SOU, T2 extends SOU, T3 extends SOU, T4 extends SOU, T5 extends SOU>
+	(...opts:[T1,T2?,T3?,T4?,T5?]): MergeMultiple<T1, T2, T3, T4, T5>
+{
+	//@ts-ignore
+	return opts.reduce((prev, cur) => mergeOptions2(prev, cur), opts[0])
 }
---
+++ src/store/main.ts
@@ -0,0 +1,103 @@
+import { StoreOptions, Store as IStore } from "./vuex-type-ext"
+import { IBlockchainSymbol, IWallet } from "./interop"
+import { TypedBlockchains, typedBlockchains } from "src/blockchains"
+import { IBlockchain } from "src/blockchains/IBlockchain"
+
+export const options: StoreOptions<IState, MutationPayloadMap, ActionPayloadMap, GettersReturnMap> = {
+	state: {
+		wallets: [],
+		currentWallet: null,
+		txToPush: null,
+		webrtc: {
+			outgoingId: 1,
+			connected: false
+		},
+	},
+	getters: {
+		txHash: (state, getters) => (tx, bc) =>
+		{
+			return getters.blockchains[bc](state.currentWallet!.chainId).getTxHash(tx)
+		},
+		txToPushHash: (state, getters) =>
+		{
+			let tx = state.txToPush
+			if (!tx)
+				return undefined
+			
+			return getters.txHash(tx.tx, tx.wallet.blockchain)
+		},
+		blockchains: (state, getters) => typedBlockchains,
+		currentBlockchain: (state, getters) => getters.blockchains[state.currentWallet!.blockchain](state.currentWallet!.chainId)
+	},
+	mutations: {
+		setWalletList: (state, payload) =>
+		{
+			console.log(`SET_WALLET_LIST: `, payload)
+			state.wallets = payload.wallets
+		},
+		setCurrentWallet: (state, payload) => state.currentWallet = payload.wallet,
+		setTxToPush: (state, payload) => state.txToPush = payload,
+		resetTxToPush: state => state.txToPush = null,
+		webrtcIncId: state => state.webrtc.outgoingId++,
+		webrtcResetId: state => state.webrtc.outgoingId = 1,
+		webrtcConnected: state =>
+		{
+			console.log('MUTATION: webrtcConnected')
+			state.webrtc.connected = true
+		},
+		webrtcDisconnected: state => state.webrtc.connected = false,
+	},
+	actions: {
+		webrtcConnected(store)
+		{
+			store.commit('webrtcConnected')
+		},
+		webrtcDisconnected(store)
+		{
+			store.commit('webrtcDisconnected')
+			store.commit('webrtcResetId')
+		},
+	},
+}
+
+export type Store = IStore<IState, MutationPayloadMap, ActionPayloadMap, GettersReturnMap>
+export type SOptions = StoreOptions<IState, MutationPayloadMap, ActionPayloadMap, GettersReturnMap>
+
+export interface IState
+{
+	wallets: IWallet[]
+	currentWallet: IWallet | null
+	txToPush: {
+		tx: string
+		wallet: IWallet
+	} | null
+	webrtc: {
+		outgoingId: number
+		connected: boolean
+	}
+}
+
+type MutationPayloadMap = {
+	setWalletList: { wallets: IWallet[] }
+	setCurrentWallet: { wallet: IWallet }
+
+	setTxToPush: { tx: string, wallet: IWallet }
+	resetTxToPush: undefined
+
+	webrtcIncId: undefined
+	webrtcResetId: undefined
+	webrtcConnected: undefined
+	webrtcDisconnected: undefined
+}
+
+type ActionPayloadMap = {
+	webrtcConnected: undefined
+	webrtcDisconnected: undefined
+}
+
+type GettersReturnMap = {
+	txHash: (tx: string, blockchain: IBlockchainSymbol) => string
+	txToPushHash?: string
+	blockchains: TypedBlockchains,
+	currentBlockchain: IBlockchain<unknown, unknown>
+}
