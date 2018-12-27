228371e5d4a647bb97b51112dfee5cc0b554d2ac
zlumer <zlumer@gmail.com>
2018-12-27 19:45:21 +0300

    eos transfer tx creation

---
+++ src/pages/blockchain/EosTransfer.vue
@@ -0,0 +1,163 @@
+<template>
+	<div>
+		<transfer-form
+			ref="txform"
+			:form="form"
+			:inputs="inputs"
+			:loading="loading"
+			:tx-json="txJson"
+			@sign="sign"
+			@cancel="cancel"
+			@change="onFormChange"
+		/>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue, { VueWithProps } from 'src/vue-ts'
+import TransferForm, { IFormInputField } from 'src/components/TransferForm.vue'
+import { IEosTxHeaders } from 'src/blockchains/eos'
+
+function validateAddress(addr: string)
+{
+	if (!addr)
+		return false
+	
+	// TODO: unit test
+	return (addr.length <= 12) && (addr.toLowerCase() == addr) && !addr.match(/[6|7|8|9|0]/)
+}
+function validateNumber(num: number)
+{
+	if (typeof num !== "number")
+		if (typeof num !== "string")
+			return false
+	
+	return !isNaN(parseFloat(num.toString()))
+}
+
+type TRefs = {
+	txform: InstanceType<typeof TransferForm>
+}
+
+export default (Vue as VueWithProps<{ $refs: TRefs }>).extend({
+	data()
+	{
+		return {
+			loading: false,
+			txHeaders: null as IEosTxHeaders | null,
+			form: {
+				to: { label: "To:", cy: "form-to", validate: validateAddress },
+				amount: { label: "Amount:", cy: "form-amount", type: 'number', validate: validateNumber },
+				usd: { label: "USD value:", cy: "form-usd", type: 'number' },
+				memo: { label: "Memo:", cy: "form-memo", type: '' },
+			},
+			tx: {
+				to: '',
+				memo: '',
+				value: '',
+			},
+			inputs: ['to', 'amount', 'usd', 'memo']
+		}
+	},
+	computed: {
+		address: function()
+		{
+			return this.wallet.address
+		},
+		wallet: function()
+		{
+			return this.$store.state.currentWallet!
+		},
+		preparedTx: function()
+		{
+			return {
+				...this.txHeaders,
+				actions: [
+					{
+						name: "transfer",
+						account: "eosio.token",
+						authorization: [{
+							actor: this.address,
+							permission: "active"
+						}],
+						data: {
+							to: this.tx.to,
+							from: this.address,
+							quantity: this.tx.value,
+							memo: this.tx.memo
+						}
+					}
+				]
+			}
+		},
+		eosMethod: function()
+		{
+			return "transfer(from:name,to:name,quantity:asset,memo:string)"
+		},
+		txJson: function()
+		{
+			console.log(this.preparedTx)
+			return JSON.stringify({ transaction: this.preparedTx, method: this.eosMethod, wallet: this.wallet })
+		},
+		eos: function()
+		{
+			return this.$store.getters.blockchains.eos(this.wallet.chainId)
+		},
+		eosPrice: function()
+		{
+			return this.$store.getters.eosPrice
+		},
+	},
+	mounted: function()
+	{
+		this.$store.dispatch('updateTokenPrice', { blockchain: 'eos' }).then(() =>
+		{
+			let amount = parseFloat(this.$refs.txform.values['amount'] + "")
+			if (!isNaN(amount))
+				this.$refs.txform.setValue('usd', amount * this.$store.getters.eosPrice)
+		})
+	},
+	methods: {
+		async sign(form: { to: string, memo: string, amount: string })
+		{
+			this.loading = true
+			this.txHeaders = await this.eos.getTxHeaders()
+			let amount = parseFloat(form.amount + "").toFixed(4)
+			this.tx = {
+				to: form.to,
+				value: `${amount} EOS`,
+				memo: form.memo
+			}
+			this.loading = false
+		},
+		onFormChange({ field, value }: { field: string, value: unknown })
+		{
+			// console.log(`form change! ${field} ${value}`, field, value)
+
+			if (!this.eosPrice)
+				return // we can't update when the price is not ready for any reason
+			
+			if (field == 'amount')
+			{
+				let val = parseFloat(value + "")
+				if (!isNaN(val))
+					this.$refs.txform.setValue('usd', val * this.eosPrice)
+			}
+			if (field == 'usd')
+			{
+				let val = parseFloat(value + "")
+				if (!isNaN(val))
+					this.$refs.txform.setValue('amount', val / this.eosPrice)
+			}
+		},
+		cancel()
+		{
+			this.loading = false
+		},
+	},
+	components: {
+		TransferForm,
+	}
+})
+</script>
+
--- src/pages/blockchain/TransferHoc.vue
+++ src/pages/blockchain/TransferHoc.vue
@@ -1,9 +1,11 @@
 <script lang="ts">
 import Vue from 'src/vue-ts'
 import EthTransfer from "./EthTransfer.vue"
+import EosTransfer from "./EosTransfer.vue"
 import { generate } from "./bbhoc"
 
 export default generate({
 	eth: EthTransfer,
+	eos: EosTransfer,
 })
 </script>
