a243b4659ba7fd5fe78b26bdc6dd96f17438ca68
Leonid Semenov <kalesin90@gmail.com>
2018-12-28 02:40:32 +0100

    add layouts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import AppLayout from './layouts/App.vue'
+import DefaultLayout from './layouts/Default.vue'
+import { createRouter } from "./router"
+import { createStore } from "./store"
+
+let store = createStore()
+let router = createRouter(store)
 
 declare global
 {
@@ -9,7 +16,12 @@ declare global
 	}
 }
 
+Vue.component('app-layout', AppLayout)
+Vue.component('default-layout', DefaultLayout)
+
 window.vm = new Vue({
+	router,
+	store,
 	el: '#app',
 	render: h => h(App)
 })
