62617ca5ede07b13cde499e36212b18e9085899d
zlumer <zlumer@gmail.com>
2019-01-15 13:03:36 +0300

    eos: default chain id

--- src/blockchains/eos.ts
+++ src/blockchains/eos.ts
@@ -152,6 +152,9 @@ const CACHE = { } as { [chainId in IChainId]: EosBlockchain }
 export function getCachedNetworkByChainId(chainId: number | string): EosBlockchain
 {
 	chainId = chainId + ""
+	if (!chainId)
+		chainId = 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906' // eos mainnet
+	
 	let chain = CACHE[chainId as IChainId]
 	if (chain)
 		return chain
