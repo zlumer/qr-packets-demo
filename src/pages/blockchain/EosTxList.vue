525464e1f7ac346def4d7b28213d9d7738c0d154
zlumer <zlumer@gmail.com>
2019-01-17 21:11:26 +0300

    css: eos tx list styles

--- src/pages/blockchain/EosTxList.vue
+++ src/pages/blockchain/EosTxList.vue
@@ -1,15 +1,29 @@
 <template>
-	<span v-if="error" data-cy="error">ERROR LOADING TRANSACTIONS: {{ error }}</span>
-	<span v-else-if="loading" data-cy="loading">Loading tx list...</span>
-	<ul v-else data-cy="tx-list">
-		<li :key="tx.hash" v-for="tx in txs">
-			hash: {{ tx.hash.substr(0, 10) + "..." }},
-			from: {{ tx.from }},
-			to: {{ tx.to }},
-			amount: {{ tx.amount }},
-			memo: {{ tx.memo }}
-		</li>
-	</ul>
+	<div class="tx-list-container">
+		<span v-if="error" data-cy="error">ERROR LOADING TRANSACTIONS: {{ error }}</span>
+		<span v-else-if="loading" data-cy="loading">Loading tx list...</span>
+
+		<table v-else data-cy="tx-list">
+			<thead>
+				<tr>
+					<th>TxHash</th>
+					<th>From</th>
+					<th>To</th>
+					<th>Amount</th>
+					<th>Memo</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr :key="tx.hash" v-for="tx in txs">
+					<td>{{ tx.hash.substr(0, 10) + "..." }}</td>
+					<td><code>{{ tx.from }}</code></td>
+					<td><code>{{ tx.to }}</code></td>
+					<td><code>{{ tx.amount }}</code></td>
+					<td class="td-memo" :title="tx.memo"><code>{{ tx.memo }}</code></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </template>
 
 <script lang="ts">
@@ -65,7 +79,38 @@ export default Vue.extend({
 		{
 			this.error = (e instanceof Error) ? e.message : e
 		}
-	},
+	}
 })
 
 </script>
+
+<style lang="scss" scoped>
+.tx-list-container {
+	background: #fff;
+	padding: 2rem;
+	box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.04);
+	border-radius: .8rem;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+	width: 100%;
+	td {
+		color: #2e3d3f;
+		padding: 1rem .5rem;
+	}
+	th {
+		color: #457b9d;
+		padding: .5rem;
+	}
+	tr {
+		border-bottom: 1px solid #b2bcb9;
+	}
+}
+td.td-memo {
+	max-width: 20vw;
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
+}
+</style>
