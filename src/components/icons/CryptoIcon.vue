9129454918b0b971ebb3c34cacb5db3f974d4807
zlumer <zlumer@gmail.com>
2019-01-18 21:28:41 +0300

    ui: crypto icons

---
+++ src/components/icons/CryptoIcon.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import Vue from 'src/vue-ts'
+import IconBtc from "./IconBtc.vue"
+import IconEos from "./IconEos.vue"
+import IconEth from "./IconEth.vue"
+import IconNeo from "./IconNeo.vue"
+
+import { generate } from "src/pages/blockchain/bbhoc"
+
+export default generate({
+	eth: IconEth,
+	btc: IconBtc,
+	eos: IconEos,
+	neo: IconNeo,
+})
+</script>
