db6bcfe001936f6a869215b332ca2e1a2c23acb9
zlumer <zlumer@gmail.com>
2019-01-20 19:22:32 +0300

    ui: get eth txs list from store

--- src/pages/blockchain/EthTxList.vue
+++ src/pages/blockchain/EthTxList.vue
@@ -3,6 +3,7 @@
 		<span v-if="error" data-cy="error">ERROR LOADING TRANSACTIONS: {{ error }}</span>
 		<span v-else-if="loading" data-cy="loading">Loading tx list...</span>
 
+		<span v-else-if="!hasTxs" data-cy="tx-list-empty">No transactions on this wallet yet.</span>
 		<table v-else data-cy="tx-list">
 			<thead>
 				<tr>
@@ -94,9 +95,6 @@ export default Vue.extend({
 	data()
 	{
 		return {
-			txs: [] as ITransaction[],
-			error: '',
-			loading: true,
 		}
 	},
 	props: {
@@ -106,30 +104,44 @@ export default Vue.extend({
 		}
 	},
 	computed: {
-		eth: function()
+		txs: function()
 		{
-			return this.$store.getters.blockchains.eth(this.wallet.chainId)
+			let txs = this.$store.getters.ethtx_getTxs(this.wallet)
+			return txs
 		},
+		hasTxs: function()
+		{
+			return !!(this.txs && this.txs.length)
+		},
+		error()
+		{
+			let e = this.$store.getters.ethtx_hasError(this.wallet)
+			return (e instanceof Error) ? e.message : e
+		},
+		loading()
+		{
+			return this.$store.getters.ethtx_isLoading(this.wallet) || !this.$store.getters.ethtx_getTxs(this.wallet)
+		}
+	},
+	watch: {
+		wallet: function()
+		{
+			this.updateTxs()
+		}
 	},
 	methods: {
 		isSelf: function(addr: string)
 		{
 			return addr.toLowerCase() == this.wallet.address.toLowerCase()
-		}
+		},
+		updateTxs: async function()
+		{
+			this.$store.dispatch('ethtx_hardUpdateTxs', { wallet: this.wallet })
+		},
 	},
-	mounted: async function ()
+	mounted: function()
 	{
-		try
-		{
-			let txList = await this.eth.loadTxList(this.wallet)
-			let unique = { } as { [hash:string]: boolean } // TODO: handle duplicate txs
-			this.txs = txList.filter(x => unique[x.hash] ? false: (unique[x.hash] = true))
-			this.loading = false
-		}
-		catch (e)
-		{
-			this.error = (e instanceof Error) ? e.message : e
-		}
+		this.updateTxs()
 	},
 })
 
