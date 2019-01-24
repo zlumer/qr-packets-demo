65dfd4cc666fed7a5a6c94c2b741ccd731bb2531
zlumer <zlumer@gmail.com>
2019-01-24 19:31:43 +0300

    core: etherscan token list loader

--- src/blockchains/etherscan.ts
+++ src/blockchains/etherscan.ts
@@ -1,24 +1,26 @@
-import { ITx, IResponse } from "./etherscan.i"
+import { ITx, IResponse, ITokenTx } from "./etherscan.i"
 
-export const mainnet = {
-	loadTxList: (address: string) => loadTxList('https://api.etherscan.io', address)
-}
-export const rinkeby = {
-	loadTxList: (address: string) => loadTxList('https://api-rinkeby.etherscan.io', address)
-}
-export const ropsten = {
-	loadTxList: (address: string) => loadTxList('https://api-ropsten.etherscan.io', address)
-}
-export const kovan = {
-	loadTxList: (address: string) => loadTxList('https://api-kovan.etherscan.io', address)
+export const mainnet = makeNetwork('https://api.etherscan.io')
+export const rinkeby = makeNetwork('https://api-rinkeby.etherscan.io')
+export const ropsten = makeNetwork('https://api-ropsten.etherscan.io')
+export const kovan = makeNetwork('https://api-kovan.etherscan.io')
+
+export type IEtherscanNetwork = ReturnType<typeof makeNetwork>
+
+function makeNetwork(host: string)
+{
+	return {
+		loadTxList: (address: string) => loadTxList(host, address),
+		getTokenTxList: (address: string) => getTokenTxList(host, address),
+	}
 }
 
 export async function loadTxList(host: string, address: string): Promise<ITx[]>
 {
 	try
 	{
-		let res = await load(host, `/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=YourApiKeyToken`)
-		let txs = res.result as ITx[]
+		let res = await load<ITx[]>(host, `/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=YourApiKeyToken`)
+		let txs = res.result
 		txs = txs.filter((val, idx, arr) => idx == arr.findIndex(x => x.hash == val.hash)) // remove duplicates
 		return txs.reverse()
 	}
@@ -30,7 +32,24 @@ export async function loadTxList(host: string, address: string): Promise<ITx[]>
 		throw e
 	}
 }
-export async function load(host: string, path: string): Promise<IResponse<unknown>>
+export async function getTokenTxList(host: string, address: string): Promise<ITokenTx[]>
+{
+	try
+	{
+		let res = await load<ITokenTx[]>(host, `/api?module=account&action=tokentx&address=${address}&startblock=0&endblock=999999999&sort=asc&apikey=YourApiKeyToken`)
+		let txs = res.result
+		txs = txs.filter((val, idx, arr) => idx == arr.findIndex(x => x.hash == val.hash))
+		return txs.reverse()
+	}
+	catch (e)
+	{
+		if (e.message === "No transactions found")
+			return []
+		
+		throw e
+	}
+}
+export async function load<T = unknown>(host: string, path: string): Promise<IResponse<T>>
 {
 	let url = `${host}${path}`
 	return fetch(url).then(x => x.json()).then(res =>
