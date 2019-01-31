e289b7719eb365d32ab184a276116ca049bb5fc2
zlumer <zlumer@gmail.com>
2019-01-31 20:19:41 +0300

    core: utils: pick() method

--- src/store/utils.ts
+++ src/store/utils.ts
@@ -4,3 +4,15 @@ export function calcWalletId(wallet: IWallet)
 {
 	return `${wallet.blockchain}/${wallet.chainId}/${wallet.address}`
 }
+
+/**
+ * https://gist.github.com/bisubus/2da8af7e801ffd813fab7ac221aa7afc
+ * 
+ * Creates an object composed of the picked object properties.
+ * @param obj The source object
+ * @param paths The property paths to pick
+ */
+export function pick<T, K extends keyof T>(obj: T, paths: K[]): Pick<T, K>
+{
+	return { ...paths.reduce((mem, key) => ({ ...mem, [key]: obj[key] }), {}) } as Pick<T, K>
+}
