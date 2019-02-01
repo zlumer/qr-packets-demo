0e69fa31146842d3496e131a3598985ecf26b117
zlumer <zlumer@gmail.com>
2019-02-01 19:54:26 +0300

    core: router: fromQuery() utility method

---
+++ src/router-tools.ts
@@ -0,0 +1,17 @@
+import Vue from 'src/vue-ts'
+
+interface IQueryGetter
+{
+	(field: string): (this: InstanceType<typeof Vue>) => string | undefined
+	<T>(field: string, map: (value: string) => T): (this: InstanceType<typeof Vue>) => T | undefined
+	<T>(field: string, map: (value: string) => T, def: T): (this: InstanceType<typeof Vue>) => T
+}
+
+export const fromQuery: IQueryGetter = <T>(field: string, map?: (value: string) => T, def?: T) => function(this: InstanceType<typeof Vue>)
+{
+	let q = this.$route.query[field] as string | undefined
+	if (typeof q === 'undefined')
+		return def
+	
+	return map ? map(q) : q
+}
