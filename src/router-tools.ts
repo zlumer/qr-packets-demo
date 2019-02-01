3e5e837a51df6944a9a45ab0ec2d1f2e3cbd3f7a
zlumer <zlumer@gmail.com>
2019-02-01 21:25:02 +0300

    core: router: getQueryString() utility method

--- src/router-tools.ts
+++ src/router-tools.ts
@@ -1,4 +1,5 @@
 import Vue from 'src/vue-ts'
+import { Route } from 'vue-router'
 
 interface IQueryGetter
 {
@@ -14,4 +15,8 @@ export const fromQuery: IQueryGetter = <T>(field: string, map?: (value: string) 
 		return def
 	
 	return map ? map(q) : q
+}
+export const getQueryString = function(route: Route)
+{
+	return route.fullPath.substr(route.path.length)
 }
