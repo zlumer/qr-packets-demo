6c57f21ffb1c68e730e5c649517b3875f34f9a16
zlumer <zlumer@gmail.com>
2019-02-09 23:38:20 +0300

    tests: skip webrtc test

--- src/network/connect.test.ts
+++ src/network/connect.test.ts
@@ -8,15 +8,19 @@ global.wrtc = require('wrtc')
 
 import { WebrtcHSInitiator, WebrtcHSResponder } from "./wrtchs"
 
-describe('webrtc handshake', () =>
+describe.skip('webrtc handshake', () =>
 {
 	it('should connect', (done) =>
 	{
+		let callbacks = [(a: () => void) => initiator.destroy(a)]
+		let finish = (d: () => void): void => callbacks.length ? callbacks.pop()!(() => finish(d)) : d()
 		let hsUrl = 'wss://duxi.io/shake'
 
 		let initiator = new WebrtcHSInitiator(hsUrl, sid =>
 		{
+			callbacks.push(a => responder.destroy(a))
+
 			let responder = new WebrtcHSResponder(hsUrl, sid, () => console.log(`CONNNNNNNNNNECTED!!!!! (responder)`))
-		}, () => (console.log(`CONNNNNNNNNNECTED!!!!! (initiator)`), done()))
+		}, () => (console.log(`CONNNNNNNNNNECTED!!!!! (initiator)`), finish(done)))
 	}, 25000)
 })
